dbb3244bb4961847dbb0b77a763ce1dd03ce03e2
LonelehKitten <scarface.theminer@gmail.com>
2021-04-16 17:30:29 -0300

    some tweaks

--- lib/reader.h
+++ lib/reader.h
@@ -4,18 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <limits.h>
 
 void init (char *);
 int read (FILE *);
 
-int getNextLine (FILE *, char **, int *);
+int getNextLine (FILE *, unsigned char **, int *);
 
 extern struct {
     int count;
 } args_;
 
-//extern void findsubstring_(char [20], char [INT_MAX]);
-extern void findsubstring_(char *, char *, int *lineToSearch_length, int *line_length);
+extern void findsubstring_(unsigned char *, unsigned char *, int *lineToSearch_length, int *line_length);
 
 #endif /* READER_H */
